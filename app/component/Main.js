087a7a41b6fc266e67c0442c545a0f01481973ff
Rani Rathod <ranirathor20@navgurukul.org>
2024-08-08 12:13:05 +0530

    Working on responsive page

--- app/component/Main.js
+++ app/component/Main.js
@@ -6,20 +6,19 @@ function Main() {
 			<div className="main-page">
 				<div className='mcq-div'>MCQ Test Portal</div>
 				<div className="instructions">
-					<div className='info'>
-					<h2>Instructions</h2>
-					<p>Please read the instructions carefully before you begin:</p>
-					<ul>
+					<div className='info1'><b>Instructions</b></div>
+					<div className='info2'>Please read the instructions carefully before you begin:</div>
+					<div className='ul-div'><ul>
 						<li>Timing: The test is timed. You have [specified time] minutes to complete all questions.</li>
 						<li>Questions: The test comprises [number of questions] multiple-choice questions (MCQs).</li>
 						<li>Navigation: You can navigate between questions using the 'Next' and 'Previous' buttons.</li>
 						<li>Selection: Choose the best answer for each question. You can change your answers anytime before submitting.</li>
 						<li>Submission: Once you have answered all questions, click the 'Submit' button. You must submit before the timer runs out.</li>
 						<li>Review: After submission, you may review your answers and scores.</li>
-					</ul>
-					<p>Please ensure a stable internet connection. If you encounter any issues, contact support immediately.</p>
-					<div>Click 'Let's Start' to begin the test.</div>
-					<div><b>Good luck!</b></div>
+					</ul></div>
+					<div className='info3'>Please ensure a stable internet connection. If you encounter any issues, contact support immediately.</div>
+					<div className='info4'>Click 'Let's Start' to begin the test.
+						<br></br><b>Good luck!</b>
 					</div>
 					<div className='line'></div>
 					<button className="start-button">Let's Start!</button>
@@ -30,4 +29,3 @@ function Main() {
 	)
 }
 export default Main;
-
