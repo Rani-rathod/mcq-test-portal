5c8384b32511bde4ead40e691015238418619b86
Rani Rathod <ranirathor20@navgurukul.org>
2024-09-19 18:09:05 +0530

    fixed 1st page

--- app/component/Header.js
+++ app/component/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div className="Header">
-      <div className='header-box'>
+      <div className='header-main'>
         <img src="/Logo.png" className="header-image" /> 
         <span>QA</span>
       </div>
