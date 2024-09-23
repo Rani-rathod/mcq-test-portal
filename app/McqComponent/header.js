a1b9e60015ac70e88e8969278946748122015d9b
Rani Rathod <ranirathor20@navgurukul.org>
2024-09-23 20:15:33 +0530

    fixed width and height

--- app/McqComponent/header.js
+++ app/McqComponent/header.js
@@ -11,14 +11,13 @@ function Headerpage() {
                             <img className="logo" src="Logo.png" alt="Logo" />
                             <span className="header-title">QA</span>
                         </div>
-                        {/* <div className='model-line'></div> */}
                         <div className='Pagination-box'>
                             <Pagination />
                         </div>
                     </div>
                     <div className="submit-button-content">
                         <a href="/SubmitComponent" className='submit-button'>Submit</a>
-                    </div>
+                    </div>  
                 </div>
 
 
